e5a0c343dd6c470c82a4d71e6dfadef3f6019857
Fabian <fabian.bentz@dotsunited.de>
2020-02-13 16:06:06 +0100

    prepare basic thumb structure

--- projects/ngx-image-overlay/src/lib/interfaces/thumbnail.ts
+++ projects/ngx-image-overlay/src/lib/interfaces/thumbnail.ts
@@ -1,2 +1,20 @@
 export interface Thumbnail {
+  alt: string;
+  src: string;
+  srcSet: '';
+  sizes: Array<ThumbnailSizes>;
+  className?: string;
+}
+
+export interface ThumbnailSrcSet {
+  alt: string;
+  src: string;
+  srcSet: '';
+  className?: string;
+}
+
+
+export interface ThumbnailSizes {
+  size: string;
+  mediaCondition?: string;
 }
